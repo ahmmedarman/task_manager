305ce773fd3f403b740f05f5cad9f5414f220cef
rasul <rasulahamed132@gmail.com>
2025-01-24 17:31:28 +0600

    add

---
+++ lib/App.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/ui/Screens/splash_screen.dart';
+
+class TaskManagerApp extends StatelessWidget {
+  const TaskManagerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      initialRoute : '/',
+      onGenerateRoute: ( RouteSettings settings) {
+      late Widget widget;
+      if(settings.name == '/') {
+        widget= const SplashScreen();
+      }
+
+      return MaterialPageRoute(builder: (_) => widget);
+      },
+
+    );
+
+}
+
+
